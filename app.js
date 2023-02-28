d236846c10662b1d110e5aeb9e4da8fcb77a7742
EcaCosca <enriquecoscarelli@gmail.com>
2023-02-28 06:40:17 -0300

    Browser migration

---
+++ app.js
@@ -0,0 +1,19 @@
+const typeString = "Hello world"
+const typeString2 = 'Hello single quotations'
+const typeString3 = `Bactics`
+
+const typeNumber = 123
+const typeNumber2 = "123"
+
+// console.log(typeof typeNumber)
+// console.log(typeof typeNumber2)
+
+const adding = typeNumber + typeNumber2
+
+// console.log(typeof adding)
+
+const typeBoolean = true
+
+const typeArray = ['string', 2, true, [1, 2, 4]]
+
+console.log(typeArray[3][1])
